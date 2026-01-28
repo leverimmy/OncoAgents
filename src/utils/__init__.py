9b61f59361ab404a8df3a938fb6a7764b910daec
root <k38663279@163.com>
2026-01-28 14:21:31 +0800

    feat: update render user profile

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,6 +1,7 @@
 from .emotion_stage import NAME2STAGE, STAGE2NAME, STAGE_E, STAGE_K, STAGE_PI, STAGE_S
 from .logger import logger
 from .safe_dict import SafeDict
+from .user_profile_renderer import render_user_profile
 
 __all__ = [
     "STAGE_PI",
@@ -11,4 +12,5 @@ __all__ = [
     "NAME2STAGE",
     "logger",
     "SafeDict",
+    "render_user_profile",
 ]
