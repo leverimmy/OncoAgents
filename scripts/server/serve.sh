da7bff0a4bf0c31cce72d1d927eac02261a84dba
leverimmy <leverimmy@gmail.com>
2026-02-08 10:42:13 +0800

    chore: update project

--- scripts/server/serve.sh
+++ scripts/server/serve.sh
@@ -1,7 +1,7 @@
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
-vllm serve /ssd/xze/Qwen3-8B \
+CUDA_VISIBLE_DEVICES=0,1,2,3 \
+vllm serve /ssd/xze/hf/models/Qwen/Qwen3-8B \
   --host 0.0.0.0 --port 8000 \
   --dtype auto \
-  --served-model-name Qwen3-8B \
-  --tensor-parallel-size 8 \
+  --served-model-name Qwen/Qwen3-8B \
+  --tensor-parallel-size 4 \
   --gpu-memory-utilization 0.90
